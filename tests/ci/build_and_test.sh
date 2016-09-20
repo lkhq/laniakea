c228eda5f53a01646072815828ae59b39cf8c6a1
Matthias Klumpp <matthias@tenstral.net>
2016-09-20 02:37:08 +0200

    Add Travis CI

---
+++ tests/ci/build_and_test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+echo "D compiler: $DC"
+set -x
+$DC --version
+
+#
+# This script is supposed to run inside the Laniakea Docker container
+# on the CI system.
+#
+
+mkdir build
+meson ..
+ninja
+
+DESTDIR=/tmp/lk-root/ ninja install
