27be3e5ba9f051b396c592ef9611aa3586a58f55
Matthias Klumpp <matthias@tenstral.net>
2016-09-20 21:40:37 +0200

    Add Britney action wrapper class

---
+++ src/spears/britney.d
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2016 Matthias Klumpp <matthias@tenstral.net>
+ *
+ * Licensed under the GNU Lesser General Public License Version 3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the license, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import std.stdio : File;
+import std.process;
+import std.array : appender;
+import std.path : baseName, buildPath;
+static import std.file;
+
+import laniakea.logging;
+import laniakea.config;
+
+/**
+ * Interface to Debian's Archive Migrator (Britney2)
+ */
+class Britney
+{
+
+private:
+
+    struct BritneyResult
+    {
+        bool success;
+        string data;
+    }
+
+    string britneyExe;
+    string britneyDir;
+
+public:
+
+    this ()
+    {
+        britneyDir = buildPath (BaseConfig.get ().workspace, "dist", "britney2");
+        britneyExe = buildPath (britneyDir, "britney.py");
+    }
+
+    private BritneyResult runBritney (const string command, const string[] args)
+    {
+        string getOutput (File f)
+        {
+            char[] buf;
+            auto output = appender!string;
+            while (f.readln (buf)) {
+                output ~= buf;
+            }
+            return output.data;
+        }
+
+        auto brArgs = [britneyExe] ~ command ~ args;
+        auto brCmd = pipeProcess (brArgs);
+
+        if (wait (brCmd.pid) != 0) {
+            return BritneyResult (false, getOutput (brCmd.stdout) ~ "\n" ~ getOutput (brCmd.stderr));
+        }
+
+        return BritneyResult (true, getOutput (brCmd.stdout));
+    }
+}
