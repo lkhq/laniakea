a962dcbb5c5b669fb4307fd635d00c72a0c06282
Matthias Klumpp <matthias@tenstral.net>
2018-03-12 07:09:34 +0100

    dataimport: Control GC runs
    This will improve performance of this code a bit. Using the GC less
    instead of this change (which makes the code use more memory
    temporarily) would of course be better in the long run.

--- src/dataimport/repodbimport.d
+++ src/dataimport/repodbimport.d
@@ -26,6 +26,8 @@ import laniakea.repository;
 import laniakea.db;
 import laniakea.db.schema.archive;
 
+import core.memory : GC;
+
 bool syncRepoData (string suiteName, string repoName = "master") @trusted
 {
     auto db = Database.get;
@@ -49,9 +51,14 @@ bool syncRepoData (string suiteName, string repoName = "master") @trusted
         assert (0, "The multiple repositories feature is not yet implemented.");
     }
 
+    // if we don't pay attention, of a 1m run, this code will spend more than 20s in the GC
+    // so, we control a bit when we run a collection cycle
+    GC.disable ();
+
     foreach (ref component; suite.components) {
         // Source packages
         repo.getSourcePackages (suite.name, component.name, session, true);
+        GC.collect ();
 
         foreach (ref arch; suite.architectures) {
             // binary packages
@@ -59,8 +66,11 @@ bool syncRepoData (string suiteName, string repoName = "master") @trusted
 
             // binary packages of the debian-installer
             repo.getInstallerPackages (suite.name, component.name, arch.name, session, true);
+
+            GC.collect ();
         }
     }
 
+    GC.enable ();
     return true;
 }
