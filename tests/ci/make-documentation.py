3f46a0ce04fa4cf05860ee80cb4ff418a531e1d4
Matthias Klumpp <matthias@tenstral.net>
2018-04-10 00:42:56 +0200

    ci: Search for stdx-allocator include path as well when building docs

--- tests/ci/make-documentation.py
+++ tests/ci/make-documentation.py
@@ -28,10 +28,12 @@ def build_include_dir_cmd(source_root):
 
     extra_inc_dirs = ['/usr/include/d/vibe/',
                       '/usr/include/d/diet/',
+                      '/usr/include/d/stdx-allocator/',
                       './src']
 
     for d in extra_inc_dirs + find_local_include_dirs(source_root):
-        incdirs.append('-I' + d)
+        if os.path.isdir(d):
+            incdirs.append('-I' + d)
     return incdirs
 
 
