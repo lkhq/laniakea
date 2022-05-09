0130106db38ce40a959a7de271c0a4617c92d8e8
Matthias Klumpp <matthias@tenstral.net>
2022-05-09 02:03:15 +0200

    msched: Scan for uploads and publish repos regularly

---
+++ src/lkscheduler/config.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2022 Matthias Klumpp <matthias@tenstral.net>
+#
+# SPDX-License-Identifier: LGPL-3.0+
+
+import os
+import shutil
+
+import toml
+
+import laniakea.typing as T
+from laniakea.localconfig import get_config_file
+
+
+class SchedulerConfig:
+    """Configuration for the maintenance scheduler daemon."""
+
+    instance = None
+
+    class __SchedulerConfig:
+        def __init__(self, fname=None):
+            if not fname:
+                fname = get_config_file('scheduler.toml')
+            self.fname = fname
+
+            cdata = {}
+            if fname and os.path.isfile(fname):
+                with open(fname) as toml_file:
+                    cdata = toml.load(toml_file)
+
+            cintervals = cdata.get('Intervals', {})
+            self._intervals_min = {}
+
+            # run Rubicon every 15min by default
+            self._intervals_min['rubicon'] = cintervals.get('rubicon', 15)
+
+            # publish repos all 4h by default
+            self._intervals_min['publish-repos'] = cintervals.get('publish-repos', 4 * 60)
+
+            # find executables
+            my_dir = os.path.dirname(os.path.realpath(__file__))
+            self._lk_archive_exe = os.path.normpath(os.path.join(my_dir, '..', 'lkarchive', 'lk-archive.py'))
+            if not os.path.isfile(self._lk_archive_exe):
+                self._lk_archive_exe = shutil.which('lk-archive')
+            if not self._lk_archive_exe:
+                raise ValueError('Unable to find `lk-archive` binary. Check your Laniakea installation!')
+
+            self._rubicon_exe = os.path.normpath(os.path.join(my_dir, '..', 'rubicon', 'rubicon'))
+            if not os.path.isfile(self._rubicon_exe):
+                self._rubicon_exe = shutil.which('rubicon')
+            if not self._rubicon_exe:
+                raise ValueError('Unable to find `rubicon` binary. Check your Laniakea installation!')
+
+        @property
+        def lk_archive_exe(self) -> T.PathUnion:
+            """Executable path for lk-archive"""
+            return self._lk_archive_exe
+
+        @property
+        def rubicon_exe(self) -> T.PathUnion:
+            """Executable path for rubicon"""
+            return self._rubicon_exe
+
+        @property
+        def intervals_min(self) -> T.Dict[str, T.Optional[int]]:
+            """Defined intervals to run the respective jobs at"""
+            return self._intervals_min
+
+    def __init__(self, fname=None):
+        if not SchedulerConfig.instance:
+            SchedulerConfig.instance = SchedulerConfig.__SchedulerConfig(fname)
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
--- src/lkscheduler/scheduler_daemon.py
+++ src/lkscheduler/scheduler_daemon.py
@@ -5,32 +5,72 @@
 # SPDX-License-Identifier: LGPL-3.0+
 
 import asyncio
-from datetime import datetime
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
 from laniakea import LocalConfig
+from laniakea.logging import log
+from lkscheduler.config import SchedulerConfig
 
 
-def tick():
-    print('The time is: %s' % datetime.now())
+def task_repository_publish():
+    """Publish the archive repositories."""
+    import subprocess
+
+    conf = SchedulerConfig()
+    log.info('Publishing all repositories')
+    subprocess.run([conf.lk_archive_exe, 'publish'], stdin=subprocess.DEVNULL, start_new_session=True, check=True)
+
+
+def task_rubicon_scan():
+    """Make Rubicon look for new uploads."""
+    import subprocess
+
+    conf = SchedulerConfig()
+    subprocess.run([conf.rubicon_exe], stdin=subprocess.DEVNULL, start_new_session=True, check=True)
 
 
 class SchedulerDaemon:
-    '''
+    """
     Laniakea maintenance scheduler daemon.
-    '''
+    """
 
     def __init__(self):
         from laniakea.db import Base, Database
 
         self._lconf = LocalConfig()
+        self._sconf = SchedulerConfig()
+        self._jobs = {}
 
         db = Database()
         jobstore = SQLAlchemyJobStore(engine=db.engine, tablename='maintenance_jobs', metadata=Base.metadata)
         self._scheduler = AsyncIOScheduler(jobstores={'default': jobstore})
-        self._scheduler.add_job(tick, 'interval', seconds=3, id='test-timed', name='Just a test')
+
+        intervals_min = self._sconf.intervals_min
+        if intervals_min['rubicon'] is not None:
+            job = self._scheduler.add_job(
+                task_rubicon_scan,
+                'interval',
+                id='rubicon',
+                name='Import data from new uploads',
+                jitter=60,
+                minutes=intervals_min['rubicon'],
+                replace_existing=True,
+            )
+            self._jobs[job.id] = job
+
+        if intervals_min['publish-repos'] is not None:
+            job = self._scheduler.add_job(
+                task_repository_publish,
+                'interval',
+                id='publish-repos',
+                name='Publish all repository data',
+                jitter=20,
+                minutes=intervals_min['publish-repos'],
+                replace_existing=True,
+            )
+            self._jobs[job.id] = job
 
     def run(self):
         self._scheduler.start()
