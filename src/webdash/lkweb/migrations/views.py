d732bfa212e41728e7a01c1a2a9293b4fe32b46f
Matthias Klumpp <matthias@tenstral.net>
2022-11-26 23:58:33 +0100

    webdash: Fix migration entry display, add multirepo support

--- src/webdash/lkweb/migrations/views.py
+++ src/webdash/lkweb/migrations/views.py
@@ -8,7 +8,7 @@ import math
 
 from flask import Blueprint, abort, render_template
 
-from laniakea.db import SpearsExcuse, SpearsMigrationTask, session_scope
+from laniakea.db import SpearsExcuse, SourcePackage, SpearsMigrationTask, session_scope
 
 from ..utils import is_uuid
 
@@ -19,35 +19,55 @@ migrations = Blueprint('migrations', __name__, url_prefix='/migrations')
 def index():
     with session_scope() as session:
         entries = session.query(SpearsMigrationTask).all()
-        disp_entries = []
+        disp_entries = {}
         for e in entries:
-            disp_entries.append({'id': e.idname, 'from': ', '.join(e.source_suites), 'to': e.target_suite})
+            if e.repo.name not in disp_entries:
+                disp_entries[e.repo.name] = []
+            disp_entries[e.repo.name].append(
+                {
+                    'repo_name': e.repo.name,
+                    'from': ', '.join([s.name for s in e.source_suites]),
+                    'to': e.target_suite.name,
+                }
+            )
 
         return render_template('migrations/index.html', migrations=disp_entries)
 
 
-@migrations.route('/excuses/<migration_id>/<int:page>')
-def excuses_list(migration_id, page):
+@migrations.route('/excuses/<repo_name>/<target_suite_name>/<int:page>')
+def excuses_list(repo_name, target_suite_name, page):
     with session_scope() as session:
 
-        migration = session.query(SpearsMigrationTask).filter(SpearsMigrationTask.idname == migration_id).one()
+        migration = (
+            session.query(SpearsMigrationTask)
+            .filter(
+                SpearsMigrationTask.repo.has(name=repo_name),
+                SpearsMigrationTask.target_suite.has(name=target_suite_name),
+            )
+            .one_or_none()
+        )
+        if not migration:
+            abort(404)
 
         excuses_per_page = 50
-        excuses_total = session.query(SpearsExcuse).filter(SpearsExcuse.migration_id == migration_id).count()
+        excuses_total = session.query(SpearsExcuse).filter(SpearsExcuse.migration_id == migration.id).count()
         page_count = math.ceil(excuses_total / excuses_per_page)
 
         excuses = (
             session.query(SpearsExcuse)
-            .filter(SpearsExcuse.migration_id == migration_id)
-            .order_by(SpearsExcuse.source_package)
+            .join(SourcePackage)
+            .filter(SpearsExcuse.migration_id == migration.id)
+            .order_by(SourcePackage.name)
             .slice((page - 1) * excuses_per_page, page * excuses_per_page)
             .all()
         )
 
+        migration_source_suites_str = ', '.join([s.name for s in migration.source_suites])
         return render_template(
             'migrations/excuses.html',
             excuses=excuses,
             migration=migration,
+            migration_source_suites_str=migration_source_suites_str,
             excuses_per_page=excuses_per_page,
             excuses_total=excuses_total,
             current_page=page,
@@ -65,5 +85,4 @@ def view_excuse(uuid):
         if not excuse:
             abort(404)
 
-        migration = {'idname': excuse.migration_id}
-        return render_template('migrations/excuse.html', excuse=excuse, migration=migration)
+        return render_template('migrations/excuse.html', excuse=excuse, migration=excuse.migration_task)
