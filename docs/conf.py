2fffdaa8782e81cfed55e39ce9baa6a325f64f2d
Matthias Klumpp <matthias@tenstral.net>
2024-01-14 19:27:11 +0100

    docs: Add readthedocs.yaml file
    This file is apparently now required.

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ import sys
 # -- Project information -----------------------------------------------------
 
 project = 'Laniakea'
-copyright = '2016-2020, Matthias Klumpp'
+copyright = '2016-2024, Matthias Klumpp'
 author = 'Matthias Klumpp'
 
 # The full version, including alpha/beta/rc tags
